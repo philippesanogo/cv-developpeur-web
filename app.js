8a3de6d26067cea91de42c2899b633b5604d3064
philippe sanogo <philippe.sanogo@gmail.com>
2021-02-11 12:52:20 +0100

    Animation du bouton burger
    ajout #btn-burger dans le toggle dans app.js
    
    ajout animation de la barre 1 et 3 pour former une fleche en position .open

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
     $("#btn-burger, #overlay, a").on('click', function() {
-        $("nav, main, body, #overlay").toggleClass("open");
+        $("nav, main, body, #overlay, #btn-burger").toggleClass("open");
     });
 });
