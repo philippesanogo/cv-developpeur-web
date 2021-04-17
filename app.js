23f6a826e014a09fc56a43e0543e628e09c63c1e
philippe sanogo <philippe.sanogo@gmail.com>
2021-04-17 10:34:35 +0200

    ajout de event btn dans app.js

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
-    $("#btn-burger, #overlay, a").on('click', function() {
+    let btn = document.querySelector('#btn-burger');
+    let main = document.querySelector('main');
+    let anchor = document.querySelectorAll('a');
+    console.log(anchor);
+
+    btn.addEventListener('click', () => {
+         main.classList.toggle('non-visible');
+    });
+
+    $("#btn-burger, #overlay, a").on('click', function () {
         $("nav, main, body, #overlay, #btn-burger").toggleClass("open");
     });
+
+
 });
